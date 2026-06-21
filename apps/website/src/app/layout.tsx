218077953e7c14f97025adb7fc64d938be600499
egger-meow <inpire.mg09@nycu.edu.tw>
2026-06-21 16:32:24 +0800

    feat: favicon

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -94,6 +94,13 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "/",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.png", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-icon.png", type: "image/png" }],
+  },
   robots: {
     index: true,
     follow: true,
